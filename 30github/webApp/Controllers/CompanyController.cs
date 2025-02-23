6502c6180f7e0de7f25ef130a518dd979c6f27f2
Aitemir <aitemirtrbkv@gmail.com>
2025-02-23 18:22:07 +0600

    Доделал контроллер Company

--- 30github/webApp/Controllers/CompanyController.cs
+++ 30github/webApp/Controllers/CompanyController.cs
@@ -1,28 +1,57 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using webApp.Data;
+using webApp.DTOs.Company;
 using webApp.Mappers;
 using webApp.Models;
+using webApp.Repository;
 
 namespace webApp.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 public class CompanyController : ControllerBase
 {
-    private readonly ApplicationDbContext _context;
+    private readonly ICompanyRepository _companyRepository;
 
-    public CompanyController(ApplicationDbContext context)
+    public CompanyController(ICompanyRepository companyRepository)
     {
-        _context = context;
+        _companyRepository = companyRepository;
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Company>>> GetCompany()
+    public async Task<IActionResult> GetCompanies()
     {
-        var companies = await _context.Companies
-            .Include(c => c.CustomerProjects)
-            .Include(c => c.ExecutorProjects).ToListAsync();
-        var companiesDto = companies.Select(c => c.ToCompanyDto()).ToList();
+        var companies = await _companyRepository.GetAllCompaniesAsync();
+        var companiesDto = companies.Select(c => c.ToCompanyDto());
         return Ok(companiesDto);
+    }
+    
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetCompanyById(int id)
+    {
+        Company? company = await _companyRepository.GetCompanyByIdAsync(id);
+        return company == null ? NotFound() : Ok(company.ToCompanyDto());
+    }
+
+    public async Task<IActionResult> CreateCompany([FromBody] CreateCompanyDto createCompanyDto)
+    {
+        if(!ModelState.IsValid) return BadRequest();
+        var company = createCompanyDto.ToCompanyFromCreate();
+        var result = await _companyRepository.CreateCompanyAsync(company);
+        return result == null ? StatusCode(500, "Не удалось сохранить компанию") : CreatedAtAction(nameof(GetCompanyById), new { id = company.Id }, result.ToCompanyDto());
+    }
+
+    public async Task<IActionResult> UpdateCompany(int id, [FromBody] UpdateCompanyDto updateCompanyDto)
+    {
+        if(!ModelState.IsValid) return BadRequest();
+        var company = await _companyRepository.UpdateCompanyAsync(id, updateCompanyDto);
+        return company == null ? NotFound() : Ok(company.ToCompanyDto());
+    }
+
+    public async Task<IActionResult> DeleteCompany(int id)
+    {
+        var company = await _companyRepository.DeleteCompanyAsync(id);
+        if(company == null) return NotFound();
+        return NoContent();
     }
 }
