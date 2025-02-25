018dcd6282cffd7d5f5d537adf8fa9741997effd
Aitemir <aitemirtrbkv@gmail.com>
2025-02-25 22:16:27 +0600

    Добавил в DI контейнер зависимости, а также использовал подключение для базы данных

--- 30github/webApp/Program.cs
+++ 30github/webApp/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using webApp.Data;
+using webApp.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
