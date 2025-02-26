797f196ab30a19a5ae6222e9a0b98666b8e8a5fd
Aitemir <aitemirtrbkv@gmail.com>
2025-02-26 21:17:37 +0600

    Добавил интерфейс для репозитория

--- 30github/webApp/Interfaces/IProjectRepository.cs
+++ 30github/webApp/Interfaces/IProjectRepository.cs
@@ -7,7 +7,7 @@ public interface IProjectRepository
 {
     Task<List<Project>> GetAllProjectsAsync();
     Task<Project?> GetProjectByIdAsync(int id);
-    Task<Project> CreateProjectAsync(Project project);
+    Task<Project?> CreateProjectAsync(Project project);
     Task<Project?> UpdateProjectAsync(int id, UpdateProjectDto updateProjectDto);
     Task<Project?> DeleteProjectAsync(int id);
 }
