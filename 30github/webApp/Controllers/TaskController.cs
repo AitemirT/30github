999c8736585a71e1cf9ef3f5b19a19f467bc13dc
Aitemir <aitemirtrbkv@gmail.com>
2025-03-02 14:22:37 +0600

    Добавил контроллер Crud для Задач в проекте

---
+++ 30github/webApp/Controllers/TaskController.cs
@@ -0,0 +1,60 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using webApp.DTOs.Task;
+using webApp.Models;
+using webApp.Repository;
+
+namespace webApp.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class TaskController : ControllerBase
+{
+    private readonly ITaskRepository _taskRepository;
+    private readonly IMapper _mapper;
+
+    public TaskController(ITaskRepository taskRepository, IMapper mapper)
+    {
+        _taskRepository = taskRepository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var tasks = await _taskRepository.GetAllAsync();
+        var taskDto = _mapper.Map<IEnumerable<TaskDto>>(tasks);
+        return Ok(taskDto);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var task = await _taskRepository.GetByIdAsync(id);
+        return task == null ? NotFound() : Ok(_mapper.Map<TaskDto>(task));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] CreateTaskDto createTaskDto)
+    {
+        if(createTaskDto == null) return BadRequest("Данные отсутствуют");
+        if (!ModelState.IsValid) return BadRequest(ModelState);
+        var task = _mapper.Map<TheTask>(createTaskDto);
+        var createdTask = await _taskRepository.CreateAsync(task);
+        return createdTask == null ? StatusCode(500, "Не удалось создать задачу") : CreatedAtAction(nameof(Get), new { id = createdTask.Id }, _mapper.Map<TaskDto>(createdTask));
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Put(int id, [FromBody] UpdateTaskDto updateTaskDto)
+    {
+        if (!ModelState.IsValid) return BadRequest();
+        var task = await _taskRepository.UpdateAsync(id, updateTaskDto);
+        return task == null ? NotFound() : Ok(_mapper.Map<TaskDto>(task));
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var task = await _taskRepository.DeleteAsync(id);
+        return task == null ? NotFound() : NoContent();
+    }
+}
