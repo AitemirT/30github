2b74c7bce1e73edc89285716366ab2f200f687ee
Aitemir <aitemirtrbkv@gmail.com>
2025-03-07 22:39:11 +0600

    Добавил валидацию в Dto задачи для создания и редактирования

--- 30github/webApp/DTOs/Task/UpdateTaskDto.cs
+++ 30github/webApp/DTOs/Task/UpdateTaskDto.cs
@@ -1,14 +1,23 @@
+using System.ComponentModel.DataAnnotations;
 using webApp.Controllers.Enums;
 
 namespace webApp.DTOs.Task;
 
 public class UpdateTaskDto
 {
+    [Required(ErrorMessage = "Название задачи обязательно к заполнению")]
     public string NameOfTask { get; set; }
+    [Required(ErrorMessage = "Описание задачи обязательно к заполнению")]
     public string Description { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "Приоритет не может быть меньше 0")]
     public int PriorityOfTheTask { get; set; }
+    [Required(ErrorMessage = "Статус задачи обязателен")]
+    [EnumDataType(typeof(StatusOfTheTask), ErrorMessage = "Статус задачи не может быть пустым")]
     public StatusOfTheTask StatusOfTheTask { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "Автор задачи обязателен")]
     public int AuthorId { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "Исполнитель задачи обязателен")]
     public int ExecutorId { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "Проект задачи обязателен")]
     public int ProjectId { get; set; }
 }
